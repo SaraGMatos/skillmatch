f66a2352df22e54dd863addb0465c6de061af60d
SaraGarciaM <saragarciamatos@gmail.com>
2024-05-20 10:44:12 +0100

    Adds Header

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import BurgerMenu from "./BurgerMenu";
+import UserDropdown from "./UserDropdown";
+
+function Header() {
+  return;
+  <>
+    <BurgerMenu />
+    <UserDropdown />;
+  </>;
+}
+
+export default Header;
