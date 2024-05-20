8a54d6ab205d4c653dffc54a3d5453ba5e76e872
SaraGarciaM <saragarciamatos@gmail.com>
2024-05-20 15:45:12 +0100

    Basic routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,23 @@
-function App() {}
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./components/LoginPage";
+import MainPage from "./components/MainPage";
+import UserPage from "./components/UserPage";
+import ConnectionsPage from "./components/ConnectionsPage";
+import Chat from "./components/Chat";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/user" element={<UserPage />} />
+        <Route path="/connections" element={<ConnectionsPage />} />
+        <Route path="/chat/:id" element={<Chat />} />
+      </Routes>
+    </>
+  );
+}
 
 export default App;
