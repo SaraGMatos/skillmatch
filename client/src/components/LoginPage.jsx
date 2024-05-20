2bc162069017e1181016cff897551a5a09fee3c5
SaraGarciaM <saragarciamatos@gmail.com>
2024-05-20 10:42:26 +0100

    Adds LoginPage

---
+++ client/src/components/LoginPage.jsx
@@ -0,0 +1,12 @@
+import LoginForm from "./LoginForm";
+
+function LoginPage() {
+  // Logo
+  // Login Form / Supabase ?
+  // Image
+  // Link to Create Account
+
+  return <LoginForm />;
+}
+
+export default LoginPage;
