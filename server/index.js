938bd4572858161e8fbe3f8d01edefb9d192aed5
Fabian <xlyncex@gmail.com>
2024-05-20 09:51:09 +0100

    Added index.js server file and dependencies

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+
+const http = require("http")
+const { Server } = require("socket.io")
+
+const cors = require("cors")
+app.use(cors())
+
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: "https://socket-test-25-50.netlify.app",
+        methods: ["GET", "POST"]
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log(`user connected: ${socket.id}`)
+
+    socket.on("join_room", (data)=> {
+        socket.join(data)
+    })
+
+    socket.on("send_message", (data) => {
+        socket.to(data.room).emit("receive_message", data)
+    })
+})
+
+server.listen(process.env.PORT || 3000, ()=> {
+    console.log("server is running")
+})
