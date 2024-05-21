58f1814d29d35b14a83023ecca7f026997df4ea8
Momo Sallu <MuhammadSallu@gmail.com>
2024-05-21 11:51:03 +0100

    Conditional rendering implemented for User Dropdown and BurgerMenu

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,19 @@
 import BurgerMenu from "./BurgerMenu";
 import UserDropdown from "./UserDropdown";
+import { useLocation } from "react-router-dom";
 
 function Header() {
+  let location = useLocation();
+  let isSignedIn = location.pathname !== "/";
+  // if (location.pathname !== "/")
   return (
     <>
-      <BurgerMenu />
-      <UserDropdown />
+      <h1>SkillMatch</h1>
+      {isSignedIn && (
+        <>
+          <BurgerMenu /> <UserDropdown />
+        </>
+      )}
     </>
   );
 }
