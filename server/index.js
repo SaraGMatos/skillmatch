8632ce22f4d78af3d6155a8e27c56408fb55bc06
Fabian <xlyncex@gmail.com>
2024-05-22 10:32:51 +0100

    Changing emit for broadcast on server for chat functionality

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ io.on("connection", (socket) => {
     })
 
     socket.on("send_message", (data) => {
-        socket.to(data.room).emit("receive_message", data)
+        socket.to(data.room).broadcast("receive_message", data)
     })
 })
 
