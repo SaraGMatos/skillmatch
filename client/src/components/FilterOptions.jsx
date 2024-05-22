ca70ebe51eb7adb5173c19e52578e0dcb9d1880a
SaraGarciaM <saragarciamatos@gmail.com>
2024-05-22 12:38:06 +0100

    Adds basic structure of FilterOptions - dynamic rendering of user interests not done

--- client/src/components/FilterOptions.jsx
+++ client/src/components/FilterOptions.jsx
@@ -1,7 +1,36 @@
+import { Link } from "react-router-dom";
+import { UserContext } from "../contexts/UserContext";
+import { useContext, useEffect } from "react";
+
 function FilterOptions() {
-  // Link to Matches
-  // Link to Learning
-  // Filter dropdown
+  const { user } = useContext(UserContext);
+  console.log(user);
+
+  // Below is incomplete
+  useEffect(() => {
+    async function getUserInterests() {
+      let { data, error } = await supabase.rpc("get_user_interests", {
+        userid: user.user_id,
+      });
+      if (error) console.error(error);
+      else console.log(data);
+    }
+  }, []);
+
+  // main/exchange?interest1
+  // main/learning?interest2
+
+  return (
+    <div>
+      <button>Matches</button>
+      <select name="filter" id="filter">
+        // User Interest List
+        <option value="interest 1">Interest 1</option>
+        <option value="interest 2">Interest 2</option>
+      </select>
+      <button>Learning</button>
+    </div>
+  );
 }
 
 export default FilterOptions;
