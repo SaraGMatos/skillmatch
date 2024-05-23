0837ce67b3b027e7012ca57cbe8653f4f9e56f4a
SaraGarciaM <saragarciamatos@gmail.com>
2024-05-23 21:36:44 +0100

    Fixes Learning bug

--- client/src/components/LearningList.jsx
+++ client/src/components/LearningList.jsx
@@ -1,63 +1,66 @@
 import MatchCard from "./MatchCard";
 import supabase from "../../config/config_file";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
 
 function LearningList() {
-  const [currentUserId, setCurrentUserId] = useState()
-  const [currentUserInterests, setCurrentUserInterests] = useState([])
-  const [usersWithReqSkill, setUsersWithReqSkill] = useState([])
- 
-  const getUserData = async () => {
-    const authId = await supabase.auth.getUser();
-        const userId = authId.data.user.id;
-    setCurrentUserId(userId)
-  }
+  const { user } = useContext(UserContext);
+  const [matchedUsers, setMatchedUsers] = useState([]);
 
+  const getUserInterests = async () => {
+    let { data, error } = await supabase.rpc("get_user_interests", {
+      userid: user.user_id,
+    });
+    if (error) {
+      console.error(error);
+      return [];
+    }
+    return data;
+  };
 
-  const getUserInterests = async () => { 
-    let { data, error } = await supabase.rpc('get_user_interests', {
-      userid: currentUserId
-    })
-    if (error) console.error(error)
-    else setCurrentUserInterests(data[0])
-  }  
-   
-  const getUsersMatchedSkills = async () => {
-    let { data, error } = await supabase.rpc('get_users_by_skill', {
-            skillid: currentUserInterests.skill_id
-    })
-    console.log(data, "data here")
-          if (error) console.error(error)
-          else setUsersWithReqSkill(data)
-  }
+  const getMatchedUsers = async () => {
+    const userInterests = await getUserInterests();
+    const usersThatSatisfyInterest = [];
+
+    for (const interest of userInterests) {
+      let { data, error } = await supabase.rpc("get_users_by_skill", {
+        skillid: interest.skill_id,
+      });
+
+      if (error) {
+        continue;
+      }
+
+      usersThatSatisfyInterest.push(
+        ...data.filter((person) => person.user_id !== user.user_id)
+      );
+    }
+
+    return usersThatSatisfyInterest;
+  };
 
   useEffect(() => {
-    const learningTab = async () => { 
-      await getUserData();
-      const userInterest = await getUserInterests();
-      await getUsersMatchedSkills(userInterest)
-      
+    if (user.user_id) {
+      getMatchedUsers().then((data) => {
+        setMatchedUsers(data);
+      });
     }
-    learningTab()   
-  }, []) 
-   
-    
-  
+  }, [user]);
+
   return (
     <ul>
-    {usersWithReqSkill.map((user) => {
-      return(
-        <MatchCard
-          avatar_url={user.avatar_url}
-          username={user.username}
-          user_id={user.user_id}
-          key={user.user_id}
-        />
-      )
-    })
-    }
-  </ul>
-  )
+      {matchedUsers.map((user) => {
+        return (
+          <MatchCard
+            avatar_url={user.avatar_url}
+            username={user.username}
+            user_id={user.user_id}
+            key={user.user_id}
+          />
+        );
+      })}
+    </ul>
+  );
 }
 
 export default LearningList;
