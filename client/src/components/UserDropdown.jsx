f1a348582ce6649ba388c28ca796a709e279e318
SaraGarciaM <saragarciamatos@gmail.com>
2024-05-24 12:07:43 +0100

    Debugs persistent user state on login

--- client/src/components/UserDropdown.jsx
+++ client/src/components/UserDropdown.jsx
@@ -1,14 +1,17 @@
 import { Link, useNavigate } from "react-router-dom";
 import "../styles/BurgerMenu.css";
 import supabase from "../../config/config_file";
+import { useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
 
 function UserDropdown() {
-  // Link to Connect
-  // Logout
+  const { setUser } = useContext(UserContext);
   const navigate = useNavigate();
+
   async function loggingOut() {
     if (confirm("Are you sure you want to log out?")) {
       const { error } = await supabase.auth.signOut();
+      setUser({});
       navigate("/");
     }
   }
