58d49a5b3108b3ec08902b66bb0747fdd2d64c79
Fabian <xlyncex@gmail.com>
2024-05-26 21:57:18 +0100

    Updated information shown on conneciton cards, chat header, and message cards. On connection cards/chat headers now shows chat title or chat with [names] by default. On Message cards shows user name and time of post

--- client/src/components/ConnectionCard.jsx
+++ client/src/components/ConnectionCard.jsx
@@ -1,12 +1,35 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, Route } from "react-router-dom";
+import supabase from "../../config/config_file";
 import '../styles/Connections.css'
 
 function ConnectionCard({chat}) {
 
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [cardTitle, setCardTitle] = useState(`Chat with `)
+
+  useEffect (()=>{
+    setIsLoading(true)
+
+    supabase.rpc('get_users_by_chat_id', {chatid: chat.chat_id})
+    .then(({data})=>{
+      setUsers(data)
+      let cardTitleText = data[0].username
+      for (let index = 1; index < data.length; index++) {
+        cardTitleText += `, ${data[index].username}`
+      }
+      setCardTitle(`Chat with ` + cardTitleText)
+    }).then(()=>{
+      setIsLoading(false)
+    })
+  }, [])
+
+  if (isLoading) return <h2>Loading...</h2>
+
   return (
     <div className="connection-card">
-      <Link to={`/chat/${chat.chat_id}`}>Chat name: {chat.chat_name}</Link>
+      <Link to={`/chat/${chat.chat_id}`} state={{users, chat}}>{chat.chat_name || cardTitle}</Link>
     </div>
 
   )
