5eca97789a82eb975659507cb46694590e73a65e
SaraGarciaM <saragarciamatos@gmail.com>
2024-05-27 14:45:21 +0100

    Adds conditional rendering of edit functionality

--- client/src/components/UserPicture.jsx
+++ client/src/components/UserPicture.jsx
@@ -8,6 +8,7 @@ function UserPicture({ userProfile, setUserProfile }) {
   const [newProfileImage, setNewProfileImage] = useState("");
   const [isAlertImageURL, setIsAlertImageURL] = useState(false);
   const { user } = useContext(UserContext);
+  const isLoggedUser = userProfile.user_id === user.user_id;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -37,14 +38,14 @@ function UserPicture({ userProfile, setUserProfile }) {
     <>
       <div className="UserPageComponent">
         <img src={userProfile.avatar_url} alt="" />
-        {userProfile.user_id === user.user_id ? (
+        {isLoggedUser && (
           <button className="editButton" onClick={handleOnClick}>
             <img
               src="https://static.thenounproject.com/png/2473159-200.png"
               alt=""
             />
           </button>
-        ) : null}
+        )}
         {isAlertImageURL ? (
           <form onSubmit={handleSubmit} className="NewProfilePhotForm">
             <label htmlFor="newUrl">new photo url</label>
