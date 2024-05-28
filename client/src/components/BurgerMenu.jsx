e2e744a813e5366faaf3c433ca9eed0441b126f3
Fabian <xlyncex@gmail.com>
2024-05-28 14:08:07 +0100

    Re-made burguer menu to simple component for easier styling

--- client/src/components/BurgerMenu.jsx
+++ client/src/components/BurgerMenu.jsx
@@ -1,17 +1,21 @@
 import { Link } from "react-router-dom";
-import { slide as Menu } from "react-burger-menu";
-import "../styles/BurgerMenu.css";
-import { useContext } from "react";
+import "../styles/NewBurgerMenu.css";
+import { useContext, useState } from "react";
 import { UserContext } from "../contexts/UserContext";
 
 function BurgerMenu() {
   const { user } = useContext(UserContext);
+  const [showMenu, setShowMenu] = useState(false)
+
   return (
-    <Menu>
-      <Link to="/main">Matches</Link>
-      <Link to="/connections">Connections</Link>
-      <Link to={`/user/${user.user_id}`}>My Profile</Link>
-    </Menu>
+    <div className="main-container">
+      <button onClick={()=>{ setShowMenu(!showMenu) }}><img src="https://i.ibb.co/xz2d2KY/hamburger-button-drop-down-list-computer-icons-navigation-bars-and-page-menu-templates-361b393044224.png"></img></button>
+      { showMenu ? <div className="show-menu">
+        <Link to="/main" className="menu-item" onClick={()=>{ setShowMenu(!showMenu)}}>Matches</Link>
+        <Link to="/connections" className="menu-item" onClick={()=>{ setShowMenu(!showMenu)}}>Connections</Link>
+        <Link to={`/user/${user.user_id}`} className="menu-item" onClick={()=>{ setShowMenu(!showMenu)}}>My Profile</Link>
+      </div> : null}
+    </div>
   );
 }
 
