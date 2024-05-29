ae9644fb8406dfeb8519d69de21a671a9fa7e455
DeeSzubert <pietryd@yahoo.com>
2024-05-29 10:03:01 +0100

    adding url path to graphics

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -13,13 +13,19 @@ function Header() {
         <div className="headerLogged">
           <BurgerMenu />
           <div className="headerImage">
-            <img src="../src/graphics/skillmatch_nobckg.png" alt="" />
+            <img
+              src="https://previews.dropbox.com/p/thumb/ACQVBMX2n7LgyBd1an-oOoX48R3B_qd1JRBb29bzQ85s5Vpqt4iYVv-He-Ox1wSXJyoJJaPEEf7aF9uhMVpiqhSU7f8gQlbxEV8IOiLFIEe1qkmI_nGoP3nBvddiItl0YJHQgf2LO5nesDbRcOFvMv2aJgl78pLys5DDpH0Ej16UMVgo0dfAwDYPxlVqyooB-s5n82JjVVcYFDYhb1C83SJqOF0OYZHqWgdlTGt1AP2dKCGDFx7kfl7wVow_5exJnIZAoEMLa5YWVTlXaVmGyuK35A4VHwZdYLe_EvpMMJMucBU9T8QHljCVhnVg5A9_ox8S_6tykod0eu0qVyruF8K0/p.png"
+              alt=""
+            />
           </div>
           <UserDropdown />
         </div>
       ) : (
         <div className="headerImage">
-          <img src="../src/graphics/skillmatch_nobckg.png" alt="" />
+          <img
+            src="https://previews.dropbox.com/p/thumb/ACQVBMX2n7LgyBd1an-oOoX48R3B_qd1JRBb29bzQ85s5Vpqt4iYVv-He-Ox1wSXJyoJJaPEEf7aF9uhMVpiqhSU7f8gQlbxEV8IOiLFIEe1qkmI_nGoP3nBvddiItl0YJHQgf2LO5nesDbRcOFvMv2aJgl78pLys5DDpH0Ej16UMVgo0dfAwDYPxlVqyooB-s5n82JjVVcYFDYhb1C83SJqOF0OYZHqWgdlTGt1AP2dKCGDFx7kfl7wVow_5exJnIZAoEMLa5YWVTlXaVmGyuK35A4VHwZdYLe_EvpMMJMucBU9T8QHljCVhnVg5A9_ox8S_6tykod0eu0qVyruF8K0/p.png"
+            alt=""
+          />
         </div>
       )}
     </nav>
