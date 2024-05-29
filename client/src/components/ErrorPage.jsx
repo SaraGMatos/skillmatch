14211e122ea5831ba371c587c24eed39b5a1aa40
SaraGarciaM <saragarciamatos@gmail.com>
2024-05-29 11:36:28 +0100

    Adds styles to error page

--- client/src/components/ErrorPage.jsx
+++ client/src/components/ErrorPage.jsx
@@ -1,12 +1,13 @@
 import Lottie from "lottie-react";
 import errorAnimation from "../graphics/error_animation.json";
+import "../styles/App.css";
 
 function ErrorPage() {
   return (
     <section className="error-page-section">
-      <h2>Oops, nothing to see here!</h2>
       <div className="error-animation">
         <Lottie animationData={errorAnimation} loop={true} />
+        <h2>Oops, nothing to see here!</h2>
       </div>
     </section>
   );
