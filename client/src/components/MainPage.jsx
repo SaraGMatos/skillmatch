46091527622c3f129e690ad9b3ba87c9a378f8a3
SaraGarciaM <saragarciamatos@gmail.com>
2024-05-30 09:51:19 +0100

    Removes avatarurl

--- client/src/components/MainPage.jsx
+++ client/src/components/MainPage.jsx
@@ -27,8 +27,6 @@ function MainPage() {
           username: `default_username${Math.floor(Math.random() * 1000 + 1)}`,
           description:
             "Please complete your profile to be able to match and connect with people!",
-          avatarurl:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKsNmCFrzGx_4sCjsUxR-YXkEKn1WylKpxzoYrtDKN2Knc3y6EGSUckAkUm3O4f6ORnLs&usqp=CAU",
         });
         setUser(data);
         if (error) {
